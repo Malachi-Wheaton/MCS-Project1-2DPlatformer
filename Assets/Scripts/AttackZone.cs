9e25ba89091c8e1e462ef18765d547139f1f5a3c
Malachi Wheaton <mwheaton@ltu.edu>
2025-02-12 12:16:17 -0500

    Beta

--- Assets/Scripts/AttackZone.cs
+++ Assets/Scripts/AttackZone.cs
@@ -2,22 +2,19 @@ using UnityEngine;
 
 public class AttackZone : MonoBehaviour
 {
-    public int damage = 1; // Amount of damage dealt by this attack
+    public int damage = 1; // Amount of damage the attack deals
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Check if the object has the "Enemy" tag
-        if (collision.CompareTag("Enemy"))
+        // Check if the object entering the hitbox is an enemy
+        EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
         {
-            Debug.Log($"Hit {collision.name}!");
-            Enemy enemy = collision.GetComponent<Enemy>(); // Get the Enemy script
-            if (enemy != null)
-            {
-                enemy.TakeDamage(damage); // Apply damage to the enemy
-            }
+            enemyHealth.TakeDamage(damage); // Deal damage to the enemy
         }
     }
 }
+
 
 
 
