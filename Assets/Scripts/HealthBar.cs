d84fbfe7ce75176732cc8c65ba3a4905b5043db1
Malachi Wheaton <mwheaton@ltu.edu>
2025-02-16 16:37:26 -0500

    Health Bar Done

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider slider;  // Reference to the UI slider
+
+    public void SetMaxHealth(int maxHealth)
     {
-        
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;  // Set the bar to full at the start
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHealth(int health)
     {
-        
+        slider.value = health; // Update the slider value
     }
 }
+
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,23 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxhealth = 10;
     public int health;
-    // Start is called before the first frame update
+    public HealthBar healthBar; // Reference to the health bar script
+
     void Start()
     {
         health = maxhealth;
+        if (healthBar != null)
+        {
+            healthBar.SetMaxHealth(maxhealth); // Initialize the health bar
+        }
     }
 
-public void TakeDamage(int damage) 
-    { 
+    public void TakeDamage(int damage)
+    {
         health -= damage;
-        if(health <= 0)
+
+        if (healthBar != null)
         {
-            Destroy(gameObject);
+            healthBar.SetHealth(health); // Update the health bar when taking damage
         }
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Add death animation, effects, or respawn logic here
+        Destroy(gameObject); // Destroy the player
     }
 }
